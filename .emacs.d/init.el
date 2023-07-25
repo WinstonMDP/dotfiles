943592a700d5679c12bed476cf9c1a5b945bb0f5
WinstonMDP <winstonmdp@gmail.com>
2023-07-25 22:50:56 +0300

    sm

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,4 +11,20 @@
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
 (add-hook 'agda2-mode-hook
-     (lambda () (define-key agda2-mode-map (kbd "C-c C-g") 'agda2-show-constraints)))
+    (lambda ()
+        (display-line-numbers-mode)
+        (define-key agda2-mode-map (kbd "C-c C-g") 'agda2-show-constraints)))
+(setq display-line-numbers-type 'relative)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
