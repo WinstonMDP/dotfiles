ba7e9704298eb564d2b6f1c2cbdad3e9f407bfde
WinstonMDP <skrimer.swat@bk.ru>
2024-07-28 21:43:45 +0300

    Add helix to the builded programs

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,17 @@
 set -g fish_greeting
 
-abbr -a hx helix
-
-set -gx VISUAL helix
+set -gx VISUAL hx
 set -gx EDITOR $VISUAL
 
 set -gx _JAVA_AWT_WM_NONREPARENTING 1
 
+set -gx GOPATH $HOME/builded/go
+
+set -gx HELIX_RUNTIME $HOME/builded/helix/runtime
+
 fish_add_path /usr/lib/rustup/bin
+fish_add_path $HOME/builded/helix/target/release
+fish_add_path $GOPATH/bin
 
 starship init fish | source
 zoxide init fish | source
