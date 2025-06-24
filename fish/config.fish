3b4491e17fe2d73380dd214e817abb6d9b44ddca
WinstonMDP <skrimer.swat@bk.ru>
2025-06-24 17:48:38 +0300

    sm

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,3 @@
-set -g fish_greeting
-
-set -gx VISUAL helix
-set -gx EDITOR $VISUAL
-
-set -gx _JAVA_AWT_WM_NONREPARENTING 1
-
-fish_add_path /usr/lib/rustup/bin
-
 starship init fish | source
 zoxide init fish | source
 
